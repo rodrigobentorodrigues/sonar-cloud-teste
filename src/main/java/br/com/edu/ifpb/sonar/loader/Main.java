49e606e8828adfde830479f2a36acfa6e600fec9
Rodrigo Bento <rodrigobentor2014@gmail.com>
2017-12-04 00:27:20 -0300

    Codigo alterado para verificacao do sonar

--- src/main/java/br/com/edu/ifpb/sonar/loader/Main.java
+++ src/main/java/br/com/edu/ifpb/sonar/loader/Main.java
@@ -1,8 +1,10 @@
+package br.com.edu.ifpb.sonar.loader;
+
 
 public class Main {
     
     public static void main(String[] args) {
-        int a, b, c, d = 0;
+        int a, b, c, e, f, g, d = 0;
         a = d;
     }
     
